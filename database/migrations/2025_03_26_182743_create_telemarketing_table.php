74d2f0bd675d2eef5ce352b9135d5d1c2a14f2a0
Dudiiss32 <mariaeduardaweber509@gmail.com>
2025-03-26 16:23:42 -0300

    new tables

---
+++ database/migrations/2025_03_26_182743_create_telemarketing_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telemarketing', function (Blueprint $table) {
+            $table->id();
+            $table->string('funcionario');
+            $table->string('cliente');
+            $table->string('telefone');
+            $table->string('curso');
+            $table->date('agendamento');
+            $table->date('visita');
+            $table->integer('teles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telemarketing');
+    }
+};
