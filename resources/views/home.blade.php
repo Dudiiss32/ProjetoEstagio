6f45b4dac0c4a3eccd57348b6bee6e3bb47af112
Dudiiss32 <mariaeduardaweber509@gmail.com>
2025-04-10 20:54:29 -0300

    auth home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,6 @@
 
 @section('content')
     <div class="container mt-5">
-            <h1 class="card-title mb-4 text-center">Bem vindo!</h1>
+            <h1 class="card-title mb-4 text-center">Bem vindo(a) {{auth()->user()->name}}! </h1>
     </div>
 @endsection
